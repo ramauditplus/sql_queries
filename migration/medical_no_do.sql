dfd43b93faf5392f71d228b3118ad4fdb0b511bb
ramauditplus <ramauditplus@hotmail.com>
2026-03-07 15:53:20 +0530

    upd

--- migration/medical_no_do.sql
+++ migration/medical_no_do.sql
@@ -2580,49 +2580,6 @@ select now() as time, 'UUID_CHANGES FOR INV_TXN BATCH_ID ENDS' as msg;
 --## INV_TXN
 
 -------------------------------------------------------------------------------------------------
----- INDEX RESTORE
--------------------------------------------------------------------------------------------------
-select now() as time, 'ADDING REQUIRED INDEX START' as msg;
---## ac_txn
-    create index on ac_txn (voucher_id);
-    create index on ac_txn (date);
-    create index on ac_txn (account_id);
-    create index on ac_txn (branch_id);
---## account
-    create index on account (val_name);
-    create index on account (transaction_enabled);
---## bank_txn
-    create index on bank_txn (date);
-    create index on bank_txn (ac_txn_id);
-    create index on bank_txn (account_id);
-    create index on bank_txn (voucher_id);
---## batch
-    create index on batch (barcode);
-    create index on batch (inventory_id, branch_id, warehouse_id);
---## bill_allocation
-    create index on bill_allocation (eff_date);
-    create index on bill_allocation (ac_txn_id);
-    create index on bill_allocation (account_id);
-    create index on bill_allocation (voucher_id);
-    create index on bill_allocation (branch_id, account_id, ref_no);
---## inv_txn
-    create index on inv_txn (voucher_id);
-    create index on inv_txn (date);
-    create index on inv_txn (inventory_id);
-    create index on inv_txn (branch_id);
-    create index on inv_txn (inventory_id, branch_id, warehouse_id, batch_no, vendor_id);
---## inventory
-    create index on inventory (val_name);
---## voucher
-    create index on voucher (date);
-    create index on voucher (voucher_no);
-    create index on voucher (branch_id);
-    create index on voucher (base_voucher_type);
-select now() as time, 'ADDING REQUIRED INDEX END' as msg;
---## last query
-delete from unit_conversion where conversion = 1;
-
--------------------------------------------------------------------------------------------------
 ---- DROP UNWANTED COLUMN, EXPRESSION, DEFAULT
 -------------------------------------------------------------------------------------------------
 select now() as time, 'DROPPING UNWANTED COLUMN & TABLE START' as msg;
@@ -3321,3 +3278,46 @@ drop index if exists inv_txn_warehouse_id_idx;
 drop index if exists inv_txn_batch_id_idx;
 --
 select now() as time, 'DROPPING TEMP INDEX END' as msg;
+
+-------------------------------------------------------------------------------------------------
+---- INDEX RESTORE
+-------------------------------------------------------------------------------------------------
+select now() as time, 'ADDING REQUIRED INDEX START' as msg;
+--## ac_txn
+    create index on ac_txn (voucher_id);
+    create index on ac_txn (date);
+    create index on ac_txn (account_id);
+    create index on ac_txn (branch_id);
+--## account
+    create index on account (val_name);
+    create index on account (transaction_enabled);
+--## bank_txn
+    create index on bank_txn (date);
+    create index on bank_txn (ac_txn_id);
+    create index on bank_txn (account_id);
+    create index on bank_txn (voucher_id);
+--## batch
+    create index on batch (barcode);
+    create index on batch (inventory_id, branch_id, warehouse_id);
+--## bill_allocation
+    create index on bill_allocation (eff_date);
+    create index on bill_allocation (ac_txn_id);
+    create index on bill_allocation (account_id);
+    create index on bill_allocation (voucher_id);
+    create index on bill_allocation (branch_id, account_id, ref_no);
+--## inv_txn
+    create index on inv_txn (voucher_id);
+    create index on inv_txn (date);
+    create index on inv_txn (inventory_id);
+    create index on inv_txn (branch_id);
+    create index on inv_txn (inventory_id, branch_id, warehouse_id, batch_no, vendor_id);
+--## inventory
+    create index on inventory (val_name);
+--## voucher
+    create index on voucher (date);
+    create index on voucher (voucher_no);
+    create index on voucher (branch_id);
+    create index on voucher (base_voucher_type);
+select now() as time, 'ADDING REQUIRED INDEX END' as msg;
+--## last query
+delete from unit_conversion where conversion = 1;
